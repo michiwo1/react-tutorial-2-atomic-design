161f0518e51b09654164cb4c65c098b3e7254627
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-17 11:56:29 +0900

    organismを作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./compnents/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./compnents/atoms/button/SecondaryButton";
 import { SearchInput } from "./compnents/molecules/SearchInput";
+import { UserCard } from "./compnents/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "テスト",
+  image: "https://source.unsplash.com/0kL5NSDH_Z0",
+  email: "1234@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }

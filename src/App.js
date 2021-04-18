71e51dab534d0d49c9934d5f23ca69800ababc20
michiwo <54099086+michiwo1@users.noreply.github.com>
2021-04-18 16:51:23 +0900

    templateを作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import { PrimaryButton } from "./compnents/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./compnents/atoms/button/SecondaryButton";
 import { SearchInput } from "./compnents/molecules/SearchInput";
 import { UserCard } from "./compnents/organisms/user/UserCard";
+
+import { DefaultLayout } from "./compnents/templates/DefaultLayout";
 import "./styles.css";
 
 const user = {
@@ -17,12 +20,14 @@ const user = {
 
 export default function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>検索</SecondaryButton>
-      <br />
-      <SearchInput />
-      <UserCard user={user} />
-    </div>
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
   );
 }
